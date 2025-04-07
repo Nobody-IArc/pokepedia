3fc4b16441e5f10247bccabbfe7f122446ba8890
Nobody-IArc <utk3970@naver.com>
2025-04-08 00:15:20 +0900

    타입 상성 정리

---
+++ src/pokemonTypes/pokemonTypes.ts
@@ -0,0 +1,7 @@
+export const pokemonTypes = [
+  'Normal', 'Fire', 'Water', 'Grass', 'Electric', 'Ice',
+  'Fighting', 'Poison', 'Ground', 'Flying', 'Psychic', 'Bug',
+  'Rock', 'Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy'
+] as const;
+
+export type PokemonType = typeof pokemonTypes[number];
